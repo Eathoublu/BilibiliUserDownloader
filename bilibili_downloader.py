dd16763c062c62f842dfa3f5af362196b8019741
Eathoublu_LanYixiao <36666145+Eathoublu@users.noreply.github.com>
2020-04-01 21:28:07 +0800

    V0.1

---
+++ bilibili_downloader.py
@@ -0,0 +1,166 @@
+import requests
+import re
+import os
+import json
+import sys
+from you_get import common as you_get
+
+class BilibiliDownloader(object):
+    def __init__(self):
+        pass
+    @staticmethod
+    def run(dic, urll):
+        for u in urll:
+            try:
+                sys.argv = ['you-get', '-o', dic, u]
+                you_get.main()
+            except Exception as e:
+                print(e)
+        return
+
+class BilibiliVideoManager(object):
+
+    def __init__(self):
+        self.rule = re.compile('<span class="cur-page">.*?/(.*?)</span></div>', re.S)
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        }
+        self.trule = re.compile('eta="true">(.*?)_哔哩哔哩', re.S)
+
+    @staticmethod
+    def genurl(url, page):
+        urls = []
+        urls.append(url)
+        for idx in range(1, page):
+            urls.append('{}?p={}'.format(url, idx+1))
+        return urls
+
+    def run(self, bv):
+
+        # url = 'https://api.bilibili.com/x/space/arc/search?mid=12473905&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp'
+        # url = 'https://www.bilibili.com/video/BV1Ez411b7jR'
+        url = 'https://www.bilibili.com/video/{}'.format(bv)
+        req = requests.get(url, headers=self.headers)
+        content = str(req.content, 'utf8')
+        wholep = self.rule.findall(content)
+        if wholep:
+            return self.genurl(url, int(wholep[0]))
+        return [url, ]
+
+    def gettitle(self, url):
+        c = str(requests.get(url, headers=self.headers).content, 'utf8')
+        title = self.trule.findall(c)[0]
+        return title
+
+
+
+
+class BilibiliUserManager(object):
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.burl2 = 'https://space.bilibili.com/{}'
+        self.i2url = 'https://api.bilibili.com/x/relation/stat?vmid={}'
+        self.burl = 'https://api.bilibili.com/x/space/acc/info?mid={}&jsonp=jsonp'
+        self.surl = 'https://api.bilibili.com/x/space/arc/search?mid={}&pn={}&ps=25&jsonp=jsonp'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        }
+        self.uid = None
+
+    def run(self, uid):
+        uid = str(uid)
+        self.uid = uid
+        abspath = self.prefix + '/' + uid
+        if not os.path.exists(abspath):
+            os.mkdir(abspath)
+            os.mkdir(abspath+'/profile')
+            os.mkdir(abspath+'/video')
+        jsoninfo0 = str(self.request(self.i2url.format(uid)), 'utf8')
+        # print(jsoninfo0)
+        jif = json.loads(jsoninfo0)
+        follower = jif['data']['follower']
+        following = jif['data']['following']
+
+        jsoninfo1 = str(self.request(self.burl.format(uid)), 'utf8')
+        jif = json.loads(jsoninfo1)
+        name = jif['data']['name']
+        sex = jif['data']['sex']
+        avataru = jif['data']['face']
+        rank = jif['data']['rank']
+        level = jif['data']['level']
+        birthday = jif['data']['birthday']
+        sign = jif['data']['sign']
+        with open(abspath+'/profile/basic.txt', 'w') as f:
+            f.write('name: {} sex:{} rank:{} level:{} birthday:{} sign:{} follower:{} following:{}'.format(name, sex, rank, level, birthday, sign, follower, following))
+            f.close()
+        avatar = self.request(avataru)
+        with open(abspath+'/profile/avatar.jpg', 'wb') as f:
+            f.write(avatar)
+            f.close()
+
+        with open(abspath+'/profile/source.txt', 'w') as f:
+            f.write('{};;{}'.format(jsoninfo0, jsoninfo1))
+            f.close()
+
+    def searchlist(self):
+        videol = []
+        pn = 1
+        while True:
+            sres = str(self.request(self.surl.format(self.uid, pn)), 'utf8')
+            sl = self.parselist(json.loads(sres))
+            if sl:
+                videol += sl
+            else:
+                break
+            pn += 1
+        return videol
+
+    @staticmethod
+    def parselist(jso, mode='b'):
+        vlist = jso['data']['list']['vlist']
+        idl = []
+        if not vlist:
+            return False
+        for v in vlist:
+            if mode=='b':
+                idl.append(v['bvid'])
+            else:
+                idl.append(v['aid'])
+        return idl
+
+    def request(self, url):
+        return requests.get(url, headers=self.headers).content
+
+class Manager(object):
+    def __init__(self, prefix='.'):
+        self.prefix = prefix
+        self.bum = BilibiliUserManager(prefix=prefix)
+        self.bvm = BilibiliVideoManager()
+        self.bd = BilibiliDownloader()
+
+    def run(self, uid):
+        self.bum.run(uid=uid)
+        vlist= self.bum.searchlist()
+        vbase = self.prefix+'/'+str(uid)+'/video/'
+        k = 0
+        tk = len(vlist)
+        for v in vlist:
+            k += 1
+            print('{}/{}'.format(k, tk))
+            svl = self.bvm.run(v)
+            tit = str(self.bvm.gettitle(svl[0])).replace('/', '\\')
+            if os.path.exists(vbase+tit):
+                pass
+            else:
+                os.mkdir(vbase+tit)
+            self.bd.run(vbase+tit, svl)
+
+if __name__ == '__main__':
+
+    m = Manager(prefix='.')
+    m.run(uid='')
+
+
+
+
+
